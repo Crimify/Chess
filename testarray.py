782d63c883b9b714f44e2363ff4e0b03ec647d22
Crimify <crimifycrim@yahoo.com>
2020-10-15 20:25:17 -0400

    Add files via upload

---
+++ testarray.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#c-castle n-knight b-bishop q-queen k-king p-pawn 1-blank sqaure 
+board = [['c','n','b','q','k','b','n','c'],['p','p','p','p','p','p','p','p']]
+
+board.insert(2, [1,1,1,1,1,1,1,1])
+board.insert(3, board[2])
+board.insert(4, board[3])
+board.insert(5, board[4])
+board.insert(6, board[1])
+board.insert(7, board[0])
+for row in board:
+    for piece in row:
+        print(piece,end=" ")
+    print()
+
